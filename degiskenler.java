452b53e25b993eafca8518a05837ff2064587556
Irem Yigit <yigitiremm@gmail.com>
2022-08-28 00:31:01 +0300

    kodlar eklendi.

---
+++ degiskenler.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class degiskenler {
+    public static void main(String[] Args){
+        Scanner scan = new Scanner(System.in);
+
+        int yas1=scan.nextInt();
+        int yas2=scan.nextInt();
+        int yas3=scan.nextInt();
+
+        System.out.println("yas1: " + yas1 + "yas2: " + yas2 + "yas3: " + yas3);
+
+
+
+    }
+}
